b2a033647fb74bc87429479ccd20ef82f63ec85a
Beth Carretta <bethcarretta@gmail.com>
2023-09-01 11:45:13 -0400

    added getFullName method

--- models/customer.js
+++ models/customer.js
@@ -1,17 +1,17 @@
 /** Customer for Lunchly */
 
-const db = require("../db");
-const Reservation = require("./reservation");
+const db = require('../db')
+const Reservation = require('./reservation')
 
 /** Customer of the restaurant. */
 
 class Customer {
   constructor({ id, firstName, lastName, phone, notes }) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.phone = phone;
-    this.notes = notes;
+    this.id = id
+    this.firstName = firstName
+    this.lastName = lastName
+    this.phone = phone
+    this.notes = notes
   }
 
   /** find all customers. */
@@ -25,8 +25,8 @@ class Customer {
          notes
        FROM customers
        ORDER BY last_name, first_name`
-    );
-    return results.rows.map(c => new Customer(c));
+    )
+    return results.rows.map((c) => new Customer(c))
   }
 
   /** get a customer by ID. */
@@ -40,23 +40,29 @@ class Customer {
          notes 
         FROM customers WHERE id = $1`,
       [id]
-    );
+    )
 
-    const customer = results.rows[0];
+    const customer = results.rows[0]
 
     if (customer === undefined) {
-      const err = new Error(`No such customer: ${id}`);
-      err.status = 404;
-      throw err;
+      const err = new Error(`No such customer: ${id}`)
+      err.status = 404
+      throw err
     }
 
-    return new Customer(customer);
+    return new Customer(customer)
+  }
+
+  /** get a customer's full name. */
+  async getFullName() {
+    const customer = await Customer.get(this.id)
+    return `${customer.firstName} ${customer.lastName}`
   }
 
   /** get all reservations for this customer. */
 
   async getReservations() {
-    return await Reservation.getReservationsForCustomer(this.id);
+    return await Reservation.getReservationsForCustomer(this.id)
   }
 
   /** save this customer. */
@@ -68,16 +74,16 @@ class Customer {
              VALUES ($1, $2, $3, $4)
              RETURNING id`,
         [this.firstName, this.lastName, this.phone, this.notes]
-      );
-      this.id = result.rows[0].id;
+      )
+      this.id = result.rows[0].id
     } else {
       await db.query(
         `UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4
              WHERE id=$5`,
         [this.firstName, this.lastName, this.phone, this.notes, this.id]
-      );
+      )
     }
   }
 }
 
-module.exports = Customer;
+module.exports = Customer
